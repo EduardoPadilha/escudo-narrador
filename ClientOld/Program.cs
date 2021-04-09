8cca2e0edc50e8bb58a16b5b9e6b7dc8e27ed85c
Eduardo Padilha <todmalkavia@gmail.com>
2021-04-09 00:49:20 -0300

    Conversão do Blazor para .NET 5

--- ClientOld/Program.cs
+++ ClientOld/Program.cs
@@ -1,20 +1,20 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using MudBlazor.Services;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Client
+namespace EscudoNarrador.Client
 {
     public class Program
     {
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
+            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
             builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
